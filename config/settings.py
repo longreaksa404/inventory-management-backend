d10d4dffd0037ff82870c37524dcad977af0dad6
reaksa <helloworldreaksa.com>
2026-01-14 12:47:52 +0700

    test

--- config/settings.py
+++ config/settings.py
@@ -152,6 +152,7 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_ACKS_LATE = True
 CELERY_WORKER_PREFETCH_MULTIPLIER = 1
 
+
 # Email configuration (production/dev)
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
