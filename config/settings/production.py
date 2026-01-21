e18432f67a2bc8dbbaa382e24312c2632b2b0ad0
reaksa404 <longreaksaofficial@gmail.com>
2026-01-21 11:45:43 +0700

    Fix production settings: add ROOT_URLCONF

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,32 +1,31 @@
 from .base import *
 import os
 
-DEBUG=False
+DEBUG = False
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+SECRET_KEY = env("SECRET_KEY")
+
+ROOT_URLCONF = "config.urls"  # <-- this was missing
 
 DATABASES = {
     "default": env.db("DATABASE_URL")
 }
 
-SECRET_KEY = env("SECRET_KEY")
-
-
-# HTTP Strict Transport Security (HSTS)
-SECURE_HSTS_SECONDS = 60 * 60 * 24 * 30  # 30 days
+# Security settings
+SECURE_HSTS_SECONDS = 60 * 60 * 24 * 30
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 SECURE_SSL_REDIRECT = True
-
 SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = "DENY"
 SECURE_REFERRER_POLICY = "same-origin"
-
-
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+# Email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
@@ -34,13 +33,12 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
+# Celery
 CELERY_BROKER_URL = env("REDIS_URL")
 CELERY_RESULT_BACKEND = env("REDIS_URL")
-
 CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
 
-
-
+# Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
